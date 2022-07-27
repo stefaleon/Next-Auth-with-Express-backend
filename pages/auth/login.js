ea1b034eec3fa2a947ac23b82bd39cfc5a552b9a
stefaleon <stefaleon@gmail.com>
2022-07-27 21:31:50 +0300

    Add the about, protected and login pages

---
+++ pages/auth/login.js
@@ -0,0 +1,5 @@
+import Layout from 'components/Layout';
+
+export default function login() {
+  return <Layout title='Login'>Login</Layout>;
+}
---
+++ pages/protected.js
@@ -0,0 +1,5 @@
+import Layout from 'components/Layout';
+
+export default function protectedPage() {
+  return <Layout>This is the protected page</Layout>;
+}
